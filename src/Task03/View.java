d58fb7b222e373aae9775aae9c8de0f1fb534aba
Roman <roma040805@gmail.com>
2023-03-07 14:37:18 +0200

    Third task

---
+++ src/Task03/View.java
@@ -0,0 +1,13 @@
+package Task03;
+
+import java.io.IOException;
+
+/**
+ * The interface View
+ */
+public interface View {
+    public String viewShow();
+    public String viewInit(String[] numbers);
+    public void viewSave() throws IOException;
+    public void viewRestore() throws Exception;
+}
